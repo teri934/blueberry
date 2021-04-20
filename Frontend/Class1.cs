506c43613579feed54b0027dd357720488417a60
Teri934 <56218654+Teri934@users.noreply.github.com>
2021-04-20 22:20:53 +0200

    Add Test and install packages

--- Frontend/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Frontend
-{
-	public class Class1
-	{
-	}
-}
---
+++ Frontend/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Frontend
+{
+	public class Client
+	{
+		static HttpClient client = new HttpClient();
+	}
+}
