0d8a144393a9d7d04a1724893788fe32a0ab6ab5
Teri934 <56218654+Teri934@users.noreply.github.com>
2021-04-21 18:34:06 +0200

    Update Test

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,12 +1,81 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Frontend;
+using Backend.Entities;
 
 namespace Test
 {
 	class Program
 	{
-		static void Main(string[] args)
+        static HttpClient client = new HttpClient();
+        static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        static void ShowUser(Data user)
+		{
+            if (user != null)
+                Console.WriteLine($"{user.Username} {user.Name} {user.Surname}");
+            else
+                Console.WriteLine("No user");
 		}
-	}
+
+        static void ShowUser(User user)
+        {
+            if (user != null)
+                Console.WriteLine($"{user.Username} {user.Name} {user.Surname}");
+            else
+                Console.WriteLine("No user");
+        }
+
+        static async Task RunAsync()
+        {
+            // Update port # in the following line.
+            //client.BaseAddress = new Uri("https://localhost:5001/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                //Get all users
+                List<User> users = await Client.GetAllUsersAsync();
+
+				foreach (User u in users)
+					ShowUser(u);
+
+				Console.WriteLine();
+
+                //Get existing user
+                Data user = await Client.GetExistingUserAsync(new SignIn {Username = "george3", Password = "123" });
+                ShowUser(user);
+
+                //check potential user
+                //bool value = await Client.CheckPotentialUserAsync(new SignUp { Username = "emma"});
+                bool value = await Client.CheckPotentialUserAsync(new SignUp { Username = "rose"});
+                Console.WriteLine(value);
+
+                user = null;
+                if(value)
+				{
+                    user = await Client.AddUserAsync(new User {Username = "rose", Name = "Emily", Surname = "Black", Password = "123"});
+				}
+                ShowUser(user);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
 }
